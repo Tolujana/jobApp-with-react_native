4cc639d893432ff347521e6ca5b367159090bb79
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-04 22:08:49 +0100

    added Welcome message

--- app/index.js
+++ app/index.js
@@ -17,8 +17,18 @@ const Home = () => {
     <SafeAreaView style={{flex: 1,
      backgroundColor:COLORS.lightWhite}}>
       <Text><Stack.Screen options={{headerStyle:{backgroundColor:COLORS.lightWhite },
-       headerShadowVisible:false, headerLeft:()=>(<ScreenHeaderBtn iconUrl={icons.menu} dimension='60%'/>), headerRight:()=>(<ScreenHeaderBtn iconUrl={icons.profile} dimension='100%'/>)}}/></Text>
+       headerShadowVisible:false, headerLeft:()=>(<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>), headerRight:()=>(<ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>), headerTitle:''}}/></Text>
+     
+    <ScrollView showVerticalScrollIndicator={false}>
+
+<View style= {{flex:1, padding: SIZES.medium}}>
+  <Welcome />
+  <Popularjobs />
+  <Nearbyjobs />
+</View>
+    </ScrollView>
     </SafeAreaView>
+    
   )
 }
 
