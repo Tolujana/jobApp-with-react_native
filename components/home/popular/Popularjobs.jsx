100d28b6f0878f8edb959bb9029b166752bdacf8
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-04 22:10:47 +0100

    popularjobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import { popularJobCard } from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <Text style={styles.headerTitle}>Popular jobs</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
