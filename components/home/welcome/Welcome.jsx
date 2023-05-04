1f560f62905374d3a94e5d55ac4d96029ff3c260
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-04 22:10:05 +0100

    completed Welcome Component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './welcome.style'
-
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlateList,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+const JobTypes = ["Full-time", "Part-time"];
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobTypes, setActiveJobTypes] = useState("full-time");
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}> Hello Jane</Text>
+        <Text style={styles.welcomeMessage}> Find your perfect Jobs</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            PlaceHolder="Search jobs"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={JobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobTypes, item)}
+              onPress={() => {
+                setActiveJobTypes(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.activeJobTypes}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          horizontal
+          contentContainerStyle={{ columnGap: SIZES.small }}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
