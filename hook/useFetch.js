b2188e36eb04f8702972447d6ee4cfe9239f5479
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-06 22:16:21 +0100

    completed useFetch

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { RAPID_API_KEY } from "@env";
+import { set } from "react-native-reanimated";
+import { useReducer } from "react/cjs/react.production.min";
 
 const KEY = RAPID_API_KEY;
+
 const useFetch = ({ endPoint, query }) => {
   const [data, setData] = useState([]);
-  const [isLoading, SetisLodading] = useState(false);
+  const [isLoading, SetIsloading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
@@ -19,4 +22,24 @@ const useFetch = ({ endPoint, query }) => {
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
+
+  const fetchData = async () => {
+    setIsloading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      SetIsloading(false);
+    } catch (error) {
+      setError(error);
+      alert("there is an error");
+      setIsloading(false);
+    }
+  };
+  useEffect(() => fetchData(), []);
+
+  const refetch = () => {
+    setIsloading(true);
+    fetchData();
+  };
+  return { data, isLoading, error, refetch };
 };
