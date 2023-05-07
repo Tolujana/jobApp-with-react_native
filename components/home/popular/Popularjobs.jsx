a8f6e4074fd715bbafe1225abebf355e83d75079
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-07 23:28:58 +0100

    connected Api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,23 +1,15 @@
 import { useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
-import PopularJobCard, {
-  popularJobCard,
-} from "../../common/cards/popular/PopularJobCard";
+import PopularJobCard, { popularJobCard } from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", { query: "React Developer", num_pages: 1 });
+  console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
