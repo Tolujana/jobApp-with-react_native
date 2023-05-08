735c93b439204855984a66a27717df37a2a1afd4
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-08 22:36:44 +0100

    changed api

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,41 +4,36 @@ import { RAPID_API_KEY } from "@env";
 import { set } from "react-native-reanimated";
 import { useReducer } from "react/cjs/react.production.min";
 
-const KEY = RAPID_API_KEY;
+const KEY = "673f251c96msh3a35e0a75b06279p1f8466jsn2f9cd918ea86";
 
 const useFetch = ({ endPoint, query }) => {
   const [data, setData] = useState([]);
-  const [isLoading, SetIsloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
-    url: `https://jsearch.p.rapidapi.com/${endPoint}`,
-    params: {
-      ...query,
-    },
-    headers: {
-      "X-RapidAPI-Key": KEY,
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-    },
+    url: `https://901522ec-fa4d-4b63-aecc-a237dc24ac90.mock.pstmn.io/jobs/`,
   };
 
   const fetchData = async () => {
-    setIsloading(true);
+    setIsLoading(true);
     try {
       const response = await axios.request(options);
-      setData(response.data.data);
-      SetIsloading(false);
+      setData(response.data);
+      setIsLoading(false);
     } catch (error) {
       setError(error);
       alert("there is an error");
-      setIsloading(false);
+      setIsLoading(false);
     }
   };
-  useEffect(() => fetchData(), []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const refetch = () => {
-    setIsloading(true);
+    setIsLoading(true);
     fetchData();
   };
   return { data, isLoading, error, refetch };
