649f0449e2e5050dbc4b0f2dfad9c510c289e11e
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-12 23:28:02 +0100

    updated popularcards

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -7,8 +7,11 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress()}>
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-        <Image source={{ uri: item.employer_logo }} />
+        <Image source={{ uri: item.employer_logo }} resizeMode="contain" style={styles.logoImage} />
       </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.emploayer_name}
+      </Text>
     </TouchableOpacity>
   );
 };
