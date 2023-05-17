db09f9bc1f91ca54073f955796b7fe22124c6edb
Tolulope Olofinjana <tolujana@gmail.com>
2023-05-17 23:50:54 +0100

    added styles to card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { View, Text, TouchableOpacityBase, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  console.log(item);
   return (
-    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress()}>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image source={{ uri: item.employer_logo }} resizeMode="contain" style={styles.logoImage} />
       </TouchableOpacity>
-      <Text style={styles.companyName} numberOfLines={2}>
-        {item.emploayer_name}
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
       </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)}>{item.job_title}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
